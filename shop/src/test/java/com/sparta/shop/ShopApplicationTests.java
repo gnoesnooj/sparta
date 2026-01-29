cdf4a2f6ecc7dd64a75542ce1c7753ee26f2d99a
306yyy <306yyy@naver.com>
2026-01-29 17:11:37 +0900

    Test: 비관적 락 동시성 테스트 코드 작성

--- shop/src/test/java/com/sparta/shop/ShopApplicationTests.java
+++ shop/src/test/java/com/sparta/shop/ShopApplicationTests.java
@@ -1,13 +1,67 @@
 package com.sparta.shop;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.sparta.shop.order.application.OrderService;
+import com.sparta.shop.order.application.dto.OrderCreateDto;
+import com.sparta.shop.product.domain.Product;
+import com.sparta.shop.product.domain.repository.ProductRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ShopApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    OrderService orderService;
 
+    @Autowired
+    ProductRepository productRepository;
+
+    @Test
+    void 비관적락_재고차감_동시성_테스트() throws InterruptedException {
+        // given
+        Product product = Product.builder()
+                .name("테스트 상품")
+                .price(1000)
+                .stock(1)
+                .isActive(true)
+                .build();
+
+        productRepository.save(product);
+
+        int user = 2;
+        int quantity = 1;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(user);
+        CountDownLatch latch = new CountDownLatch(user);
+        // when
+        for (int i = 0; i < user; i++) {
+            executorService.submit(() -> {
+                try {
+                    orderService.create(
+                            new OrderCreateDto(product.getId(), quantity)
+                    );
+                    System.out.println("구매 성공");
+                } catch (Exception e) {
+                    System.out.println("재고 부족");
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        // then
+        Product result = productRepository.findById(product.getId()).orElseThrow();
+
+        System.out.println("남은 재고 = " + result.getStock());
+
+        assertThat(result.getStock()).isGreaterThanOrEqualTo(0);
+    }
 }
